42b19e58f287e555f1eb0d8aa101b2ea54eaa6d4
aravinds13 <aravindtvm13@gmail.com>
2024-05-06 02:33:29 -0400

    added gzip compression to backend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require("jsonwebtoken");
+const compression = require('compression'); //for gzip
 const app = express();
 
 const cors = require ('cors');
@@ -16,6 +17,7 @@ const baseUrl = process.env.BASE_URL;
 const frontendPort = process.env.FRONTEND_PORT;
 
 app.use(cors());
+app.use(compression()); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
