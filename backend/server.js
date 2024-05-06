755255e100e982e39b6e9533ee083ab9398a8051
aravinds13 <aravindtvm13@gmail.com>
2024-05-05 23:50:36 -0400

    update  cors

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const jwt = require("jsonwebtoken");
 const app = express();
 
-// const cors = require ('cors');
+const cors = require ('cors');
 const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
@@ -20,18 +20,18 @@ const frontendPort = process.env.FRONTEND_PORT;
 // app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', `${baseUrl}:${frontendPort}`);
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
-//     next();
-// })
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.setHeader('Access-Control-Allow-Origin', `${baseUrl}:${frontendPort}`);
+    res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
     next();
-});
+})
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Methods', 'POST');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Credentials', 'true');
+//     next();
+// });
 
 app.post('/api/v1/login', async (req, res) => {
 
