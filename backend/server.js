d770542c8698119f8c94e5c8636e9a48c1b27239
aravinds13 <aravindtvm13@gmail.com>
2024-05-05 23:02:35 -0400

    update headers

--- backend/server.js
+++ backend/server.js
@@ -14,11 +14,14 @@ const mongoUrl = process.env.MONGO_URL;
 const mongodbName = process.env.MONGO_DB_NAME;
 const mongoCollectionName = process.env.MONGO_COLLECTION_NAME;
 const secretKey = process.env.SECRET_KEY;
+const baseUrl = process.env.BASE_URL; 
+const frontendPort = process.env.FRONTEND_PORT;
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', `${baseUrl}:${frontendPort}`);
     res.setHeader('Access-Control-Allow-Headers', 'Content-type, Authorization');
     next();
 })
@@ -26,6 +29,7 @@ app.use((req, res, next) => {
 app.post('/api/v1/login', async (req, res) => {
 
     const response = await getUserInfo(req);
+    console.log(response);
     if(response && response.password === req.body.password){
         const token = generateToken(response.name, response.email);
         res
@@ -51,6 +55,8 @@ app.post('/api/v1/signup', async (req, res) => {
     const password = req?.body?.password;
 
     const doesUserExist = await getUserInfo(req);
+
+    console.log(doesUserExist);
 
     if(doesUserExist){
         res
@@ -106,6 +112,7 @@ app.post('/api/v1/signup', async (req, res) => {
 app.post('/api/v1/get-user-info', async (req, res) => {
 
     let response = await getUserInfo(req);
+    console.log(response);
     if(response){
         res
             .status(200)
@@ -123,23 +130,29 @@ app.post('/api/v1/get-user-info', async (req, res) => {
 });
 
 app.post('/api/v1/update-user-info', async (req, res) => {
+    console.log(req.body);
     const client = new MongoClient(mongoUrl);
     try{
         await client.connect();
         const db = client.db(mongodbName);
         const collection = db.collection(mongoCollectionName);
+        console.log(req?.body?.email);
         let email = req?.body?.email
         const query = {email};
         const updatedValues = [...req?.body.months];
         collection.updateOne(
-            query,
-            { $set: { months: updatedValues } }
+            query, // Filter by email
+            { $set: { months: updatedValues } } // Update the entire months array
           ).then(result => {
+            console.log('Document updated successfully');
             res.json(result);
+            // Close the connection
             client.close();
           })
           .catch(error => {
+            console.error('Error updating document:', error);
             res.json(error);
+            // Close the connection
             client.close();
           });
     }
@@ -167,11 +180,13 @@ const getUserInfo = async (req) => {
         await client.connect();
         const db = client.db(mongodbName);
         const collection = db.collection(mongoCollectionName);
+        console.log(req?.body?.email);
         let email = req?.body?.email
         const query = {email};
         const document = await collection.findOne(query);
         client.close();
         if(document && document.email === email){
+            console.log(document);
             return document;
         }
         else{
